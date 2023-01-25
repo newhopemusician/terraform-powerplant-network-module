0c66bb19e25d4c46d20d894820e78a9e57fb8a52
newhopemusician <95931293+newhopemusician@users.noreply.github.com>
2023-01-25 16:24:36 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "main_vpc_cidr" {
+	default = "10.0.0.0/24"
+}
+
+variable "public_subnets" {
+	default = "10.0.0.128/26"
+}
+
+variable "private_subnets" {
+	default = "10.0.0.192/26"
+}
+
+variable "region" {
+	default = "us-east-2"
+}
